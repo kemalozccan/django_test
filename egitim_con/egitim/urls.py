0cb6e432cd628db6fdc52f5ffb88420d12a3cb41
Kemal Özcan <kemalozccan@gmail.com>
2023-06-20 17:57:12 +0300

    courses uygulaması oluşturuldu

--- egitim_con/egitim/urls.py
+++ egitim_con/egitim/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Anasayfa")
 
 urlpatterns = [
+    path('',home),
     path('admin/', admin.site.urls),
 ]
