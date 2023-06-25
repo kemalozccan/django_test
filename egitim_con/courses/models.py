a2e5f30922f3c28979c68dbd4e2b77bddfbcdb15
Kemal Özcan <kemalozccan@gmail.com>
2023-06-25 15:12:36 +0300

    SlugField ile otomatik url oluşturma.

--- egitim_con/courses/models.py
+++ egitim_con/courses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
@@ -8,6 +9,12 @@ class Course(models.Model):
     imageUrl = models.CharField(max_length=50, blank=False)
     date = models.DateField()
     isActive = models.BooleanField()
+    slug = models.SlugField(default="",null=False)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super().save(args,kwargs)
+
 
     def __str__(self):
         return f"{self.title} {self.date}"
