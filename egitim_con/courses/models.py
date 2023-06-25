04e2aee3081d5282aac3b89c39a1f23ef7b13947
Kemal Özcan <kemalozccan@gmail.com>
2023-06-26 01:08:52 +0300

    Bire çok ilişkide kayıt ekleme

--- egitim_con/courses/models.py
+++ egitim_con/courses/models.py
@@ -3,13 +3,21 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+    slug=models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f"{self.name}"
+
 class Course(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     imageUrl = models.CharField(max_length=50, blank=False)
-    date = models.DateField()
+    date = models.DateField(auto_now=True)
     isActive = models.BooleanField()
     slug = models.SlugField(default="",blank=True, editable=False,null=False, unique=True,db_index=True)
+    category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
@@ -19,10 +27,5 @@ class Course(models.Model):
     def __str__(self):
         return f"{self.title} {self.date}"
     
-class Category(models.Model):
-    name = models.CharField(max_length=40)
-    slug=models.CharField(max_length=50)
-    
-    def __str__(self):
-        return f"{self.name}"
+
     
