27c27653d99d1415573986a09ae77b224b616598
Kemal Özcan <kemalozccan@gmail.com>
2023-06-26 02:04:27 +0300

    Admin panelinin bire çok ilişkide uygulama

--- egitim_con/courses/models.py
+++ egitim_con/courses/models.py
@@ -5,7 +5,7 @@ from django.utils.text import slugify
 
 class Category(models.Model):
     name = models.CharField(max_length=40)
-    slug=models.CharField(max_length=50)
+    slug=models.SlugField(default="",null=False,unique=True,db_index=True,max_length=50)
     
     def __str__(self):
         return f"{self.name}"
@@ -16,13 +16,8 @@ class Course(models.Model):
     imageUrl = models.CharField(max_length=50, blank=False)
     date = models.DateField(auto_now=True)
     isActive = models.BooleanField()
-    slug = models.SlugField(default="",blank=True, editable=False,null=False, unique=True,db_index=True)
+    slug = models.SlugField(default="",blank=True, null=False, unique=True,db_index=True)
     category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE, related_name="kurslar")
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(args,kwargs)
-
 
     def __str__(self):
         return f"{self.title} {self.date}"
