1334fd8de56a177d5deddcb00b73308b88635c71
Kemal Özcan <kemalozccan@gmail.com>
2023-06-25 20:42:53 +0300

    Admin panelinin yönetim seviyesinde özelleştirilmesi. Panelde görünecek sütunlar, verilere link verilmesi, filtreleme, arama çubuğu vs.

--- egitim_con/courses/admin.py
+++ egitim_con/courses/admin.py
@@ -2,5 +2,15 @@ from django.contrib import admin
 from .models import Course,Category
 # Register your models here.
 
-admin.site.register(Course)
-admin.site.register(Category)
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display=("title","isActive","slug",)
+    list_display_links=("title","slug",)
+    readonly_fields=("slug",)
+    list_filter=("title","isActive",)
+    list_editable=("isActive",)
+    search_fields = ("title","description")
+    
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
