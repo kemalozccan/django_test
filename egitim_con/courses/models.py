9cbc3d3950c11da38115b20536fed6735ada43a6
Kemal Özcan <kemalozccan@gmail.com>
2023-06-25 20:07:21 +0300

    Admin panelinin aktif edilmesi ve oluşturulan modellerin admin paneline eklenmesi.

--- egitim_con/courses/models.py
+++ egitim_con/courses/models.py
@@ -9,7 +9,7 @@ class Course(models.Model):
     imageUrl = models.CharField(max_length=50, blank=False)
     date = models.DateField()
     isActive = models.BooleanField()
-    slug = models.SlugField(default="",null=False, unique=True,db_index=True)
+    slug = models.SlugField(default="",blank=True, editable=False,null=False, unique=True,db_index=True)
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
@@ -22,4 +22,7 @@ class Course(models.Model):
 class Category(models.Model):
     name = models.CharField(max_length=40)
     slug=models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f"{self.name}"
     
