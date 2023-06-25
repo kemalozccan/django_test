d81b9d12f8abc4b75d0085251c8d7801e448ca6c
Kemal Özcan <kemalozccan@gmail.com>
2023-06-26 01:38:51 +0300

    Bire çok ilişkide kayıt sorgulama

--- egitim_con/courses/models.py
+++ egitim_con/courses/models.py
@@ -17,7 +17,7 @@ class Course(models.Model):
     date = models.DateField(auto_now=True)
     isActive = models.BooleanField()
     slug = models.SlugField(default="",blank=True, editable=False,null=False, unique=True,db_index=True)
-    category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, default=1, on_delete=models.CASCADE, related_name="kurslar")
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
